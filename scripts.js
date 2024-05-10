e327ab535f6ff1500df6122978b54d3e5e510a8c
Sabelo Mawela <156898027+SABELOMAWELA@users.noreply.github.com>
2024-05-10 07:35:21 +0200

    scripts.js
    Adding some CSS style for when an error is encountered and using regex to determine whether the dividend and divider contain any letters or symbols and adding the try and catch

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+ result.innerHTML= "NO calculation performed"
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor(dividend / divider);
-});
-result.innerHTML= "NO calculation performed"
+   const regex = /[a-zA-Z\W_]/g;
 
- 
+try{ 
 if (dividend === "" || divider === ""){
   result.innerText = "Division not performed. Both values are required in inputs. Try again"
   return
@@ -17,5 +17,14 @@ if (dividend === "" || divider === ""){
 
  if (parseInt(divider) == 0){
    result.innerText = "Division not performed. Invalid number provided. Try again"
-console.error("Division not performed. Invalid number provided. Try again");
- };
+throw Error("Division not performed. Invalid number provided. Try again"); };
+
+    if (regex.test(dividend)|| regex.test(divider)){
+result.classList.add("critical-error")
+ result.innerHTML = "Something critical went wrong. Please reload the page"
+ }throw new Error("Something critical went wrong. Please reload the page");
+ } catch(error){
+console.error(error.stack)
+}
+
+ });
